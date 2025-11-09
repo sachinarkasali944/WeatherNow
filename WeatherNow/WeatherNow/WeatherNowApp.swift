cf3f82d22677b023c642062677af99e8458a6a65
Sachin Arkasali <sachinarkasali762@gmail.com>
2025-11-09 15:45:56 +0530

    Initial project setup.

---
+++ WeatherNow/WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Sachin Arkasali on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherNow/WeatherNowTests/WeatherNowTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherNowTests.swift
+//  WeatherNowTests
+//
+//  Created by Sachin Arkasali on 09/11/25.
+//
+
+import Testing
+
+struct WeatherNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
